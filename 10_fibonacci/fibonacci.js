afe8b72457fc844723ce4a502522b0bf4cf5f4f3
4hmedsalah <ahmedsalah.tech@proton.me>
2025-05-25 20:21:49 +0300

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+    if (count < 0) return "ERROR";
+    if (count === 0) return 0;
 
+    let previousNumber = 0;
+    let currentNumber = 1;
+
+    for (let i = 2; i <= count; i++) {
+        let nextNumber = previousNumber + currentNumber;
+        previousNumber = currentNumber;
+        currentNumber = nextNumber;
+    }
+
+    return currentNumber;
 };
 
 // Do not edit below this line
